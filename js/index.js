7896f74bd853476879ac55516a906b07ffa39992
R6NNN <eloisewang152@gmail.com>
2023-01-30 00:15:14 +0800

    version_1.0.0

---
+++ js/index.js
@@ -0,0 +1,23 @@
+
+
+// ---------------Swiper-------------------
+
+var mySwiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  pagination: {
+      el: '.swiper-pagination',
+  },
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+  },
+  scrollbar: {
+      el: '.swiper-scrollbar',
+  },
+})
+
+
+
+
+
