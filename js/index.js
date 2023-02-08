ace303b4466d827bb490d678e676c4200bfa4c18
Downager <YiJiun.Li.C@gmail.com>
2023-02-08 15:42:26 +0000

    Deploying to master from @ 2C2R/BitBeer.com@d09f075846e550d6d6706ce9a70893a5fde26faa 🚀

--- js/index.js
+++ js/index.js
@@ -2,22 +2,58 @@
 
 // ---------------Swiper-------------------
 
-var mySwiper = new Swiper('.swiper', {
-  direction: 'horizontal',
-  loop: true,
-  pagination: {
-      el: '.swiper-pagination',
-  },
-  navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-  },
-  scrollbar: {
-      el: '.swiper-scrollbar',
-  },
-})
+// var mySwiper = new Swiper('.swiper', {
+//     direction: 'horizontal',
+//     loop: true,
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+//     scrollbar: {
+//         el: '.swiper-scrollbar',
+//     },
+// })
 
 
+//-------Exchange order-dialog------------
+
+// let btn1 = document.querySelector("#traConfirm");
+// let confirmPop = document.querySelector("#confirmPop");
+let overflow = document.querySelector("body");
+// btn1.addEventListener("click", function(){
+    
+//     overflow.style.overflow = "hidden";
+//     confirmPop.showModal();
+//   })
+
+//----------------FAQ-popup------------
+
+// 點選出現 #faq-pop (display=block)
+// 點選 #faq-sec-off  #faq-pop(display = none)
+
+let btn2 = document.querySelector("#faq-sec");
+let faqPop = document.querySelector("#faq-pop");
+let faqOff = document.getElementById("faq-sec-off");
+
+  btn2.addEventListener("click",function(){
+    
+    faqPop.style.display = "block";
+    overflow.style.overflow = "hidden";
+
+  });
+
+  faqOff.addEventListener("click",function(){
+    faqPop.style.display = "none";
+
+  })
+
+// let test = document.querySelector(".test");
+// test.addEventListener("click",function(){
+//     console.log('123');
+// })
 
 
 
